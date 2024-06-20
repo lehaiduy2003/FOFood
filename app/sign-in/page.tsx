4e30697b10666a4011c16966650990e7a8ca1dbd
Duy <lehaiduy2003@gmail.com>
2024-06-20 18:23:31 +0700

    design login page ui

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,40 +1,19 @@
+'use client'
+import MediaLogin from '../components/media-login';
+import TranditionnalLogin from "../components/traditional-login";
 
-import GetOTP from '../components/OTP-input-button'
 const LoginPage = () => {
     return (
         <>
-            <div className="relative py-16 bg-gradient-to-br from-sky-50 to-gray-200">
-                <div className="relative container m-auto px-6 text-gray-500 md:px-12 xl:px-40">
-                    <div className="m-auto md:w-8/12 lg:w-6/12 xl:w-6/12">
-                        <div className="rounded-xl bg-white shadow-xl">
-                            <div className="p-6 sm:p-16">
-                                <div className='flex justify-center flex-col'>
-                                    <GetOTP />
-                                </div>
-                                <div className="flex justify-center mt-12">
-                                    <p>OR</p>
-                                </div>
-                                <div className="mt-2 grid space-y-4">
-                                    <button className="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 
- hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100">
-                                        <div className="relative flex items-center space-x-4 justify-center">
-                                            <img src="https://tailus.io/sources/blocks/social/preview/images/google.svg" className="absolute left-0 w-5" alt="google logo" />
-                                            <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">Continue with Google</span>
-                                        </div>
-                                    </button>
-                                    <button className="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 
-                                     hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100">
-                                        <div className="relative flex items-center space-x-4 justify-center">
-                                            <img src="https://upload.wikimedia.org/wikipedia/en/0/04/Facebook_f_logo_%282021%29.svg" className="absolute left-0 w-5" alt="Facebook logo" />
-                                            <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">Continue with Facebook</span>
-                                        </div>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <main className="flex justify-center items-center flex-col min-h-screen bg-slate-100">
+                <label className='text-3xl font-bold text-center mb-5'>Sign in</label>
+                <form id="container" className='flex items-center flex-col shadow-xl w-3/4 lg:w-1/2 xl:w-1/3 2xl:w-1/4 h-auto py-10 px-5 rounded-md bg-white'>
+                    <TranditionnalLogin />
+                    <button type='submit' className="btn btn-primary btn-sm mt-5 w-full bg-blue-500 hover:bg-blue-700">Sign in</button>
+                    <label className="divider divider-neutral">OR</label>
+                    <MediaLogin />
+                </form>
+            </main>
         </>
     )
 }
