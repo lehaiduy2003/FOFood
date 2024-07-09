17b32a7f8250bd30037dd5c3095070e509a1e91d
Duy <lehaiduy2003@gmail.com>
2024-07-09 18:22:21 +0700

    update CRUD api for food

--- app/api/[id]/my-restaurant/[slug]/route.ts
+++ app/api/[id]/my-restaurant/[slug]/route.ts
@@ -24,7 +24,7 @@ export async function GET(request: NextRequest) {
       where: {
         restaurant: {
           user: {
-            id: userId, //"clxzkzq7c000010rj9ot7wikb"
+            id: userId,
           },
         },
       },
