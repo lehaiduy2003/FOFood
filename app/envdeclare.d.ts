5cc6bb8d39dd20ea5797b2becf34833d113b50d3
Duy <lehaiduy2003@gmail.com>
2024-07-09 17:20:06 +0700

    declare hostname and port for using env variables

--- app/envdeclare.d.ts
+++ app/envdeclare.d.ts
@@ -1,6 +1,10 @@
+/// <reference types="node" />
+/// <reference types="react" />
 declare namespace NodeJS {
   export interface ProcessEnv {
     GOOGLE_CLIENT_ID: string;
+    HOSTNAME: string;
+    PORT: number;
     // Add other environment variables as needed
   }
 }
