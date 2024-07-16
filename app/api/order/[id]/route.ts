f587f56403d8ab4ca6f33aa10bb10368fecc8147
Duy <lehaiduy2003@gmail.com>
2024-07-16 13:20:53 +0700

    declare order api

--- app/api/order/[id]/route.ts
+++ app/api/order/[id]/route.ts
@@ -1,19 +1,17 @@
 import { FOOD_NOT_FOUND } from "@/app/constants/foodError";
 import prisma from "@/prisma/db";
-import { Beverage, Order, OrderDetails } from "@prisma/client";
+import { Beverage, Order } from "@prisma/client";
 import { NextRequest } from "next/server";
 
-export async function POST(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
+export async function POST(request: NextRequest) {
+  const beverageId = request.nextUrl.pathname.split("/")[3];
   try {
     const req = await request.json();
     console.log(req);
 
     const beverage: Beverage | null = await prisma.beverage.findUnique({
       where: {
-        id: params.id,
+        id: beverageId,
       },
     });
 
@@ -27,30 +25,34 @@ export async function POST(
         { status: 404 }
       );
     }
+
     const order: Order = await prisma.order.create({
       data: {
-        userId: req.userId,
-        paymentMethod: req.paymentMethod,
-        total: beverage?.price * req.quantity || null,
-      },
-    });
-    const orderDetail: OrderDetails = await prisma.orderDetails.create({
-      data: {
-        orderId: order.id,
+        name: req.name,
+        address: req.address,
+        phone: req.phone,
         quantity: req.quantity,
-        beverageId: params.id,
+        beverageId: beverageId,
       },
     });
-
+    await prisma.beverage.update({
+      where: {
+        id: beverageId,
+      },
+      data: {
+        orderCount: {
+          increment: 1,
+        },
+      },
+    });
     const invoice = {
-      orderId: order.id,
+      id: [order.phone, order.beverageId],
       beverage: beverage.name,
       beverageImage: beverage.image,
-      date: orderDetail.orderAt,
-      total: order.total,
+      date: order.orderAt,
+      total: beverage.price * order.quantity,
       orderStatus: order.status,
     };
-
     return Response.json({ data: invoice }, { status: 200, statusText: "OK" });
   } catch (error: any) {
     console.log(error);
