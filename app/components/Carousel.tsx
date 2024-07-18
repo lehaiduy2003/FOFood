779a412de1f3adb691c5c72afc5d7dc2f4654ffc
Duy <lehaiduy2003@gmail.com>
2024-07-18 17:38:17 +0700

    fix img tag using next image component

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -1,11 +1,16 @@
+import Image from "next/image";
+
 const Carousel = () => {
   return (
     <>
       <div className="carousel w-full">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
-            className="w-full"
+        <div id="slide1" className="carousel-item relative w-full h-[500px]">
+          <Image
+            src="/1.png"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+            alt="1"
           />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide4" className="btn btn-circle">
@@ -16,10 +21,13 @@ const Carousel = () => {
             </a>
           </div>
         </div>
-        <div id="slide2" className="carousel-item relative w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
-            className="w-full"
+        <div id="slide2" className="carousel-item relative w-full h-[500px]">
+          <Image
+            src="/2.png"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+            alt="2"
           />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide1" className="btn btn-circle">
@@ -30,10 +38,13 @@ const Carousel = () => {
             </a>
           </div>
         </div>
-        <div id="slide3" className="carousel-item relative w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
-            className="w-full"
+        <div id="slide3" className="carousel-item relative w-full h-[500px]">
+          <Image
+            src="/3.png"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+            alt="3"
           />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide2" className="btn btn-circle">
@@ -44,10 +55,13 @@ const Carousel = () => {
             </a>
           </div>
         </div>
-        <div id="slide4" className="carousel-item relative w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
-            className="w-full"
+        <div id="slide4" className="carousel-item relative w-full h-[500px]">
+          <Image
+            src="/4.png"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+            alt="4"
           />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide3" className="btn btn-circle">
