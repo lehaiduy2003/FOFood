94c8e0230438ccf616c3cbd30ac79d8fbccff3c7
Duy <lehaiduy2003@gmail.com>
2024-07-18 15:32:49 +0700

    modify useFetchData using api key

--- app/hooks/useFetchData.ts
+++ app/hooks/useFetchData.ts
@@ -1,12 +1,21 @@
+import { Base64 } from "js-base64";
 import { useEffect, useState } from "react";
-
 export const UseFetchData = (url: string, method: string) => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(url, { method: method });
+        const response = await fetch(url, {
+          method: method,
+          headers: {
+            "Content-Type": "application/json",
+            apiKey: Base64.encode("Basic " + process.env.NEXT_PUBLIC_API_KEY),
+          },
+        });
+        if (!response.ok) {
+          throw new Error("bad response");
+        }
         const jsonData = await response.json();
 
         setData(jsonData);
@@ -17,7 +26,7 @@ export const UseFetchData = (url: string, method: string) => {
     };
 
     fetchData();
-  }, [url, method]);
+  }, [method, url]);
 
   return data;
 };
