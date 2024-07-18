5f667a2cc53860fb9efc03a366a91820f5bce170
Duy <lehaiduy2003@gmail.com>
2024-07-18 15:32:05 +0700

    declare api key

--- app/envdeclare.d.ts
+++ app/envdeclare.d.ts
@@ -5,6 +5,7 @@ declare namespace NodeJS {
     GOOGLE_CLIENT_ID: string;
     HOSTNAME: string;
     PORT: number;
+    NEXT_PUBLIC_API_KEY: string;
     // Add other environment variables as needed
   }
 }
